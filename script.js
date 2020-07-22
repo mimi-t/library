25bf39cdf4df6342b2d2bd5aa56ccf790313d1fe
Mimi Tran <7123mimit@gmail.com>
2020-07-22 16:24:57 +1000

    add 'remove book' functionality

--- script.js
+++ script.js
@@ -30,10 +30,17 @@ pages: '415',
 read: true
 }
 //----------------------------------------------delete later-------------------------------------------------
+
+/*
+--------------------------------------------------
+RUNS BEFORE TO FILL PAGE WITH CONTENT
+--------------------------------------------------
+*/
+
 const addBook_form = document.querySelector('#add-book');
 const submitForm_button = document.querySelector('#submit-form');
 const toggleForm_button = document.querySelector('#toggle-form');
-const addFormWrapper_div = document.querySelector('#add-form-wrapper')
+const addFormWrapper_div = document.querySelector('#add-form-wrapper');
 const libraryWrapper_div = document.querySelector('.library-wrapper');
 const libraryArray = [book1, book2, book3, book4];
 
@@ -45,25 +52,38 @@ const Book = function(title, author, pages, read) {
     this.read = read;
 }
 
-//clear library wrapper inner HTML
+//clear library wrapper's contents
 const clearLibraryWrapper = () => {
     libraryWrapper_div.innerHTML = "";
+}
+
+const removeBook = (index) => {
+    //delete book from libraryArray at that index, render updated array
+    libraryArray.splice(index, 1);
 }
 
 //loops through libraryArray, formats info of each book, add into HTML
 const render = () => {
     clearLibraryWrapper();
-    libraryArray.forEach((book) => {
+    libraryArray.forEach((book, index) => {
         let book_div = document.createElement('div');
         book_div.className = 'book'
+        book_div.dataset.index = index;
         book_div.innerHTML = `<div class="title"><h2>${book.title}</h2></div>
         <div class="author"><h3>${book.author}</h3></div>
         <div class="pages"><p>${book.pages} pages</p></div>
         <div class="button-bar">
-            <button class="${book.read ? 'already-read' : 'to-read'}">${book.read ? 'Already read' : 'To read'}</button>
+            <button class="read-button ${book.read ? 'already-read' : 'to-read'}">${book.read ? 'Already read' : 'To read'}</button>
             <button class="bin-button"><img src="images/trash.svg" alt="bin icon"></button>
         </div>`;
         libraryWrapper_div.appendChild(book_div);
+
+        //adds event listenter for removing books
+        book_div.querySelector('.bin-button').addEventListener('click', (e) => {
+            let index = e.currentTarget.parentNode.parentNode.dataset.index;
+            removeBook(index);
+            render();
+        })
     });
 }
 
@@ -79,17 +99,17 @@ toggleForm_button.addEventListener('click', (e) => {
     }
 })
 
-//adds a book to the library
+//adds a book to the libraryArray
 const addBookToLibrary = function(form) {
     let readStatus = form['read-status'].value === "already-read" ? true : false;
     libraryArray.push(new Book(form.title.value, form.author.value, form.pages.value, readStatus));
 }
 
+//triggers when 'add book' button is clicked (adds book to libraryArray and updates HTML)
 submitForm_button.addEventListener('click', (e) => {
     event.preventDefault();
     addBookToLibrary(addBook_form.elements);
     render();
 });
 
-render();
-  
+render();  
